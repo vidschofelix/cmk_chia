29ce2ab0a8b86ba8b0efc187d2d41a5a39bccfa7
Felix <vidschofelix@users.noreply.github.com>
2023-03-31 02:32:14 +0200

    Update chia_challengecount.sh
    fix challegecount

--- farmer/chia_challengecount.sh
+++ farmer/chia_challengecount.sh
@@ -2,6 +2,6 @@
 debuglog=/data/docker/chia/data/mainnet/log/debug.log # adjust this to your needs
 
 lastfullminute=$(date --date='60 seconds ago' '+%Y-%m-%dT%H:%M')
-count=$(grep $lastfullminute $debuglog | grep "plots were eligible for farming" | wc -l)
+count=$(grep -a $lastfullminute $debuglog | grep "plots were eligible for farming" | wc -l)
 
 echo "P \"Chia: Challenges last minute\" challgenges_per_minute=$count;5:;4:;0;10 $count challenges at $lastfullminute"
